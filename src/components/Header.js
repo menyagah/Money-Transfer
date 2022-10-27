8521ae6e8fb67366e6525f28630bd1a153f6ca91
menyagah27 <menyagah27@gmail.com>
2022-10-28 01:58:50 +0300

    Add Images
     - Create button component
     - Style header button
     - Add header image
     - Compile scss to css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import '../Css/Main.css'
+import shopping from '../img/shopping.png';
+import lady from '../img/lady.png';
 
 function Header() {
   return (
     <header className='header'>
       <div className='row'>
           <div className='col-1-of-2'>
-            <div className='header__title'>
+            <div className='header__logo-box '>
+              <div className='header__logo'>
+                PaY<span className="highlight">Me</span><span className='period'>.</span>
+              </div>
+            </div>
+            <div className='header__title u-margin-top'>
                 <h1>Fast <span className="highlight">Secure</span> Reliable<br/>
                 <span className="highlight"> Simple </span>
                 </h1>
                 <br/>
-                <h4>A simpler and secure way of making payments.</h4>
+                <h4>A simpler and secure way of making payments.<br/> We'll handle your worldwide transactions, so you can fast-track global expansion.</h4>
+                <a href='#' className='btn-text'>Learn More &darr;</a>
             </div>
           </div>
-          <div className='col-1-of-2'>
-            col 1 of 2
+          <div className='col-1-of-2 '>
+            <div className='header__image u-margin-top'>
+              <div className='composition'>
+            
+                <img src={shopping} alt='' className='composition__photo composition__photo--p3' />
+                
+              </div>
+            </div>
           </div>
       </div>
     </header>
