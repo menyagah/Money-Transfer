5dff5de2ce84069d59aa0a5da9ad2f13990cbd4d
menyagah27 <menyagah27@gmail.com>
2022-10-27 14:07:36 +0300

    Add landing page components

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Css/Main.css'
+
+function Header() {
+  return (
+    <header className="header">This is the top Header</header>
+  )
+}
+
+export default Header
