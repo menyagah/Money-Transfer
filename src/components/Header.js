e159259ef42e0a43ba8221b13920b2441bc0ba3f
menyagah27 <menyagah27@gmail.com>
2022-10-27 21:12:56 +0300

    Remove white space and add custom grid to header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,22 @@ import '../Css/Main.css'
 
 function Header() {
   return (
-    <header className="header">This is the top Header</header>
+    <header className='header'>
+      <div className='row'>
+          <div className='col-1-of-2'>
+            <div className='header__title'>
+                <h1>Fast <span className="highlight">Secure</span> Reliable<br/>
+                <span className="highlight"> Simple </span>
+                </h1>
+                <br/>
+                <h4>A simpler and secure way of making payments.</h4>
+            </div>
+          </div>
+          <div className='col-1-of-2'>
+            col 1 of 2
+          </div>
+      </div>
+    </header>
   )
 }
 
