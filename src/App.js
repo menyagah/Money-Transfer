36809d097052f72b4f62c1c8be26fb8cf90f628e
menyagah27 <menyagah27@gmail.com>
2022-10-28 07:51:58 +0300

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Grid from "./components/Grid";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Header/>
       <Grid/>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navigation'>
+      <input type={'checkbox'} className='navigation__checkbox' id='navi-toggle' />
+      <label for="navi-toggle" className='navigation__button'>
+        <span className='navigation__icon'>&nbsp;</span>
+      </label>
+      <div className='navigation__background'>&nbsp;</div>
+      <nav className='navigation__nav'>
+        <ul className='navigation__list'>
+          <li className='navigation__item'><a href='#' className='navigation__link'>Sign Up</a></li>
+          <li className='navigation__item'><a href='#' className='navigation__link'>Login</a></li>
+          <li className='navigation__item'><a href='#' className='navigation__link'>Features</a></li>
+          <li className='navigation__item'><a href='#' className='navigation__link'>Contact Us</a></li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
